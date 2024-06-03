e0dddcc1bf3683d76b6e36f2db29c1e7ccf9046c
compscidan <166721683+compscidan@users.noreply.github.com>
2024-06-03 09:06:09 -0400

    edit

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 function order() {
 
-
-
+alert( "hello")
+/*
     var num_spaghetti = document.getElementById("Spaghetti").value;
     num_spaghetti = Number(num_spaghetti);
     var num_shrooms = document.getElementById("Shrooms").value;
@@ -11,6 +11,6 @@ function order() {
     var cost = (num_spaghetti * 10.99 + num_shrooms * 12.99 + num_carbonara * 11.99) * 1.03 ;
     round(cost, 2)
     alert( "Your order will cost $" + cost);
-    
-    }
+ */   
+    } 
     
