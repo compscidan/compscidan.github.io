d3697f9b1bc11728bbf854d5ca73f7598fefdb56
compscidan <166721683+compscidan@users.noreply.github.com>
2024-06-04 11:47:44 -0400

    edit

--- myscript.js
+++ myscript.js
@@ -8,8 +8,8 @@ function order() {
     var num_carbonara = document.getElementById("Carbonara").value;
     num_carbonara = Number(num_carbonara);
     var cost = (num_spaghetti * 10.99 + num_shrooms * 12.99 + num_carbonara * 11.99) * 1.03 ;
+    round(cost, 2);
     alert (cost);
-    round(cost, 2)
     alert( "Your order will cost $" + cost);
    
     } 
