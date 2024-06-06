24fba67a86f2d902a0270e519474dc1eb173ad88
compscidan <166721683+compscidan@users.noreply.github.com>
2024-06-06 08:26:50 -0400

    edit

--- myscript.js
+++ myscript.js
@@ -1,16 +1,16 @@
 function order() {
 
 
-    var num_spaghetti = document.getElementById("Spaghetti").value;
+    let num_spaghetti = document.getElementById("Spaghetti").value;
     num_spaghetti = Number(num_spaghetti);
     alert (num_spaghetti)
-    var num_shrooms = document.getElementById("Shrooms").value;
+    let num_shrooms = document.getElementById("Shrooms").value;
     num_shrooms = Number(num_shrooms);
     alert (num_shrooms)
-    var num_carbonara = document.getElementById("Carbonara").value;
+    let num_carbonara = document.getElementById("Carbonara").value;
     num_carbonara = Number(num_carbonara);
     alert (num_carbonara)
-    var cost = (num_spaghetti * 10.99 + num_shrooms * 12.99 + num_carbonara * 11.99) * 1.03 ;
+    let cost = (num_spaghetti * 10.99 + num_shrooms * 12.99 + num_carbonara * 11.99) * 1.03 ;
     alert (cost);
     round(cost, 2);
     alert( "Your order will cost $" + cost);
